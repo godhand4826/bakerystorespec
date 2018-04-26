00494fd5fc8fdcdf4c47bc8c729fb30b26bb8261
Eric Haung <godhand4826@mail.fcu.edu.tw>
2018-04-27 00:11:24 +0800

    implement index page
    - install jquery for bootstrap
    - upgrade bootstrap to 4.1.0
    - vertical tab with static content
    - logo image
    - store name

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules', 'bootstrap','dist')));
+app.use('/jquery', express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
